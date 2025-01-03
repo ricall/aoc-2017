699c4a9b92641ffc98f00fe7ef708f5003b2980d
ricall <richard.m.allwood@gmail.com>
2025-01-04 04:52:35 +1000

    Initial Commit

---
+++ src/test/kotlin/Template.kt
@@ -0,0 +1,28 @@
+package org.ricall.dayxx
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class Template {
+    private val TEST_DATA = """""".trimMargin()
+
+    @Test
+    fun `part 1 test data`() {
+        assertEquals(1, 0)
+    }
+
+    @Test
+    fun `part 1`() {
+        TODO()
+    }
+
+    @Test
+    fun `part 2 test data`() {
+        TODO()
+    }
+
+    @Test
+    fun `part 2`() {
+        TODO()
+    }
+}
